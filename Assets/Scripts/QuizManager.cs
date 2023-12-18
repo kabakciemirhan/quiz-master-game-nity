e9716bef901c3ec7e774db29606fe5513785eb5b
Emirhan Kabakcı <kabakcemirhan@gmail.com>
2023-12-18 22:02:24 +0300

    added for loop for answers
    Repeat an event until some condition is met
    Very powerful for counting or iterating
    One type of loop is called a “For Loop”
    Loop a set number of times
    
    	for(int i = 0; i < n; i++)
    	{
    		//do stuff
    }
    
    	int i = 0 parts is runs once before the code block is executed
    		sets up the iterator
    i < n part is defines the loop condition, tells the loop when to stop, be careful of infinite loops.
    i++ part is executes at the end of every loop, used to increment or decrement the iterator.

--- Assets/Scripts/QuizManager.cs
+++ Assets/Scripts/QuizManager.cs
@@ -7,9 +7,16 @@ public class QuizManager : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI questionText;
     [SerializeField] QuestionSO questionSO;
+    [SerializeField] GameObject[] answerButtons;
 
     void Start()
     {
         questionText.text = questionSO.GetQuestion();
+
+        for (int i = 0; i < answerButtons.Length; i++)
+        {
+            TextMeshProUGUI buttonText = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>(); //butonu al, buton içindeki child objesini yani doğal olarak texti al ve değiştir.
+            buttonText.text = questionSO.GetAnswer(i);
+        }
     }
 }
