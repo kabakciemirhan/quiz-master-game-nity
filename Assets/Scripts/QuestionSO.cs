f3c9d0ae7324ad66bd3fcb13f2e02c8f043a17de
Emirhan Kabakcı <kabakcemirhan@gmail.com>
2023-12-18 21:35:37 +0300

    updated questionso.cs with arrays & getter methods

--- Assets/Scripts/QuestionSO.cs
+++ Assets/Scripts/QuestionSO.cs
@@ -7,12 +7,25 @@ public class QuestionSO : ScriptableObject
 {
     [TextArea(2,6)]
     [SerializeField] string question = "Enter new question text here";
+    [SerializeField] string[] answers = new string[4]; //4 tane element olacak yani cevaplar.
+    [SerializeField] int correctAnswerIndex; //index dememizin sebebi array lar 0 dan başladığı için.
 
     public string GetQuestion()
     {
         return question; //string değeri ile açtığımız metot, buradan istediğimiz fonksiyona geri döndürebiliriz.
     }
+
+    public string GetAnswer(int index)
+    {
+        return answers[index]; //doğru cevabı array den indexine göre çekiyor.
+    }
+
+    public int GetCorrectAnswerIndex()
+    {
+        return correctAnswerIndex; //doğru cevabın indexini çekiyor.
+    }
 }
+
 /*
 public class Test
 {
